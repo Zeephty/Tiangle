e6fdd5dc5971f07a3c1fc34d34fd6be62f61a171
Zeephty <t.flogi.ho@gmail.com>
2024-11-30 17:14:38 +0700

    1 commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt6.QtGui import QPainter, QColor
+from random import randrange
+import ui
+
+ 
+class MyWidget(QMainWindow, ui.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(0, 0, 400, 400)
+        self.setMouseTracking(True)
+
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.toggle)
+
+        self.b = False
+    
+    def toggle(self):
+        self.b = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.b:
+            painter = QPainter(self)
+            painter.setBrush(QColor("yellow"))
+            randr = randrange(2, 300)
+            painter.drawEllipse(randrange(0, self.width() - randr), randrange(0, self.height() - randr), randr, randr)
+            self.b = False
+
+ 
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
